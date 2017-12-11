bb27ad138ecf4bc556c4c92292f4b4b2a0c1256b
boronflex <kyle_palmer@icloud.com>
2017-12-11 16:35:37 -0600

    setting up a game set up

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var randomWord = require("./constructors/randomWord");
 var Word = require("./constructors/parseWord");
 var Letter = require("./constructors/letters");
 
+
+//################################game set up
 //pulls random word from the random word constructor
 
 var pullRandomWord = new randomWord;
@@ -12,24 +14,55 @@ function getNewWord(){
   pullRandomWord = pullRandomWord.newRandomWord;
 };
 
-getNewWord();
-
-
 //parses words
 var verifyWord = new Word(pullRandomWord);
 
 verifyWord.addBlanks();
-
 
 //verifies letter
 var verLetter = new Letter;
 
 verLetter.word = pullRandomWord;
 
-
 //set number of guesses from difficulty of word (by length)
 
 var guesses = pullRandomWord.length * 2;
+
+function setUp(){
+  getNewWord();
+
+}
+
+//###################################game set up
+
+function startOver(){
+
+  inquirer.prompt([
+    
+      {
+        type: "confirm",
+        name: "userInput",
+        message: "Do you want play again",
+      }
+  
+    
+    ]).then(function(command) {
+
+      switch (command.userInput) {
+        case true:
+          getNewWord();
+          runGame();
+          break;
+
+        case false:
+          console.log("ok- maybe later")
+          break;
+
+      }
+
+    });
+
+}
 
 function runGame(){
 
@@ -37,7 +70,7 @@ function runGame(){
 
   console.log(`\nGuesses left: ${guesses}`);
 
-  console.log(`${verLetter.word}`)
+  console.log(`${verLetter.word}`) //for the TA's turn this on if you want to se the word, otherwise this game is hard as hell
   
 
   if (guesses > 0 && verifyWord.blankWord.indexOf('_') !== -1){
@@ -79,13 +112,12 @@ function runGame(){
   } else if (verifyWord.blankWord.indexOf('_') === -1){
 
     console.log("You win");
+    startOver();
   
   } else {
     
     console.log("Game Over");
-    //inquirer asking if want to play again
-      //if yes run game again
-      //if no end app
+    startOver();
   }
 
 }
